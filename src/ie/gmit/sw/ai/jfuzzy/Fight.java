40cd66566f652f731de7875e1a00f29e74e9e654
Vladislavs Marisevs <vmarisevs@gmail.com>
2016-03-19 13:21:57 +0000

    all possible fuzzyfication combinations

--- src/ie/gmit/sw/ai/jfuzzy/Fight.java
+++ src/ie/gmit/sw/ai/jfuzzy/Fight.java
@@ -3,6 +3,7 @@ package ie.gmit.sw.ai.jfuzzy;
 import net.sourceforge.jFuzzyLogic.FIS;
 import net.sourceforge.jFuzzyLogic.FunctionBlock;
 import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.Variable;
 
 public class Fight {
 
@@ -18,5 +19,32 @@ public class Fight {
         FunctionBlock functionBlock = fis.getFunctionBlock("Fight");
         
         JFuzzyChart.get().chart(functionBlock);
+        
+        /*
+         *  monster_strength 		low		average		high
+         *  						->2		3			7->
+         * 
+         * 	weapon_power	none	low		average		high
+         * 					->1		1 4 6	7			7->
+         * 
+         *  life_force		dead	alive
+         *  				->20	40->
+         */
+        
+        /*
+         * Testing:
+         * Rule 1: weak monster, powerful weapon = excellent victory
+         */
+        fis.setVariable("monster_strength", 0);
+        fis.setVariable("weapon_power", 10);
+        fis.setVariable("life_force", 100);
+        fis.evaluate();        
+        Variable victory = functionBlock.getVariable("victory");
+        System.out.println("Rule 1 excellent victory : " + victory.getValue());
+        
+        
+        
+        //JFuzzyChart.get().chart(victory, victory.getDefuzzifier(), true);
+
 	}
 }
